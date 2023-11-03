c7181acb7a8ba314104370dfcc824af55d5109bd
Shivam Sharma <120017051+shivam2419@users.noreply.github.com>
2023-11-03 18:56:08 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x7##5m+5uksz^q!*kq(%ol2y5&hp0^7ruv@p4@22cqzc5&b#lp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
